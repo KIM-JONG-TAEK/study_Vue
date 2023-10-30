dcae53942b0e4af327c5bd17c8a5ca21e25ef63d
Kerwin <58503913+KIM-JONG-TAEK@users.noreply.github.com>
2023-10-30 11:02:12 +0900

    update

--- Two-way Binding/basics-05-using-the-native-event-object/app.js
+++ Two-way Binding/basics-05-using-the-native-event-object/app.js
@@ -2,12 +2,54 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: ''
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
+  },
+  computed: {
+    fullname() {
+      console.log('Running again...')
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    }
+  },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    outputFullname() {
+      console.log('Running again...')
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Kim';
+    },
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -18,6 +60,7 @@ const app = Vue.createApp({
     },
     resetInput() {
       this.name = '';
+      this.lastName = '';
     }
   }
 });
