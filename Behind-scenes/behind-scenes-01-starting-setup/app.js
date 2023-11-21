5af0cad6d7a7329f2926420c8a14197ad3b93210
Kerwin <58503913+KIM-JONG-TAEK@users.noreply.github.com>
2023-11-21 09:34:19 +0900

    Behind-scenes

---
+++ Behind-scenes/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+
+// ...
+
+let message = 'Hello!';
+
+let longMessage = message + 'World!';
+
+console.log(longMessage);
+
+message = 'Hello!!!!';
+
+console.log(longMessage);
